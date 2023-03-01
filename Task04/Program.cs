fc77c769c8cd785a563e03000e601961aa50c789
Akulich Illia <jagabunga32@gmail.com>
2023-03-02 02:07:47 +0300

    Task04 - выполнена

---
+++ Task04/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое целое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите третье целое число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+
+if (numberB > max)
+{
+    max = numberB;
+    Console.WriteLine ($"Число {max} максимальное");
+}
+if (numberC > max)
+{
+    max = numberC;
+    Console.WriteLine ($"Число {max} максимальное");
+}    
